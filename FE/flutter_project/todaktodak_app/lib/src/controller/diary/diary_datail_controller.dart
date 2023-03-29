6c6e0c37481ca4f973a4923e2426ecb74e23a604
김지환 <wlghks960@naver.com>
2023-03-29 17:54:34 +0900

    230329_FE_KIM_MODI_DIARY_TOKEN

--- FE/flutter_project/todaktodak_app/lib/src/controller/diary/diary_datail_controller.dart
+++ FE/flutter_project/todaktodak_app/lib/src/controller/diary/diary_datail_controller.dart
@@ -1,10 +1,12 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 import 'package:test_app/src/services/diary/diary_services.dart';
 
+import '../../components/analysis/feel_relation_bar_chart.dart';
 import '../../model/chart/individual_bar.dart';
 import '../../model/diary/get_diary_detail_result.dart';
 import '../../model/diary/selected_image.dart';
-import 'diary_controller.dart';
 
 class DiaryDetailController extends GetxController {
   var diaryDetailData = Data().obs;
@@ -15,7 +17,7 @@ class DiaryDetailController extends GetxController {
     "assets/images/score4.png",
     "assets/images/score5.png",
   ].obs;
-
+  final storage = const FlutterSecureStorage();
   final RxList<SelectedImage> images = [
     SelectedImage(imagePath: "assets/images/happy.png"),
     SelectedImage(imagePath: "assets/images/embarr.png"),
@@ -43,7 +45,7 @@ class DiaryDetailController extends GetxController {
   @override
   void onInit() {
     final diaryId = Get.parameters["diaryId"];
-    // getDiaryDetail(diaryId);
+    getDiaryDetail(diaryId);
     super.onInit();
   }
 
@@ -58,30 +60,63 @@ class DiaryDetailController extends GetxController {
   }
 
   getDiaryDetail(var id) async {
+    final accessToken = await storage.read(key: "accessToken");
+    final refreshToken = await storage.read(key: "refreshToken");
+    final refreshTokenExpirationTime =
+        await storage.read(key: "refreshTokenExpirationTime");
     try {
-      var data = await DiaryServices().getDiaryDetail(id);
-      if (data.state == 200) {
-        diaryDetailData.value = data.data!;
-        diaryDetailData.value.diaryEmotion!.sort();
-        diaryDetailData.value.diaryMet!.sort();
-        print("데이터 저장했다 $diaryDetailData");
-      }
-    } catch (e) {
-      print(e);
+      var dio = await DiaryServices().diaryDetailDio(
+          accessToken, refreshToken, refreshTokenExpirationTime, id);
+
+      final response = await dio.get("/diary/$id");
+    } on DioError catch (e) {
+      logger.e(e.response?.statusCode);
+      logger.e(e.response?.data);
+      logger.e(e.message);
     }
   }
 
   deleteDiary(var id) async {
+    final accessToken = await storage.read(key: "accessToken");
+    final refreshToken = await storage.read(key: "refreshToken");
+    final refreshTokenExpirationTime =
+        await storage.read(key: "refreshTokenExpirationTime");
     try {
-      var data = await DiaryServices().deleteDiary(id);
-      if (data.state == 200) {
-        Get.snackbar("삭제", "해당 일기 삭제 완료하였습니다.");
-
-        Get.delete<DiaryDetailController>();
-        Get.offNamed("/dashboard");
-      }
-    } catch (e) {
-      print(e);
+      var dio = await DiaryServices()
+          .diaryDio(accessToken, refreshToken, refreshTokenExpirationTime);
+      final response =
+          await dio.put("/diary/delete", data: {"diaryId": int.parse(id)});
+    } on DioError catch (e) {
+      logger.e(e.response?.statusCode);
+      logger.e(e.response?.data);
+      logger.e(e.message);
     }
   }
+  // getDiaryDetail(var id) async {
+  //   try {
+  //     var data = await DiaryServices().getDiaryDetail(id);
+  //     if (data.state == 200) {
+  //       diaryDetailData.value = data.data!;
+  //       diaryDetailData.value.diaryEmotion!.sort();
+  //       diaryDetailData.value.diaryMet!.sort();
+  //       print("데이터 저장했다 $diaryDetailData");
+  //     }
+  //   } catch (e) {
+  //     print(e);
+  //   }
+  // }
+
+  // deleteDiary(var id) async {
+  //   try {
+  //     var data = await DiaryServices().deleteDiary(id);
+  //     if (data.state == 200) {
+  //       Get.snackbar("삭제", "해당 일기 삭제 완료하였습니다.");
+
+  //       Get.delete<DiaryDetailController>();
+  //       Get.offNamed("/dashboard");
+  //     }
+  //   } catch (e) {
+  //     print(e);
+  //   }
+  // }
 }
--- FE/flutter_project/todaktodak_app/lib/src/services/diary/diary_services.dart
+++ FE/flutter_project/todaktodak_app/lib/src/services/diary/diary_services.dart
@@ -6,9 +6,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 import 'package:test_app/src/controller/dashboard/dashboard_controller.dart';
-import 'package:test_app/src/model/diary/delete_diary_result.dart';
-import 'package:test_app/src/model/diary/get_diary_detail_result.dart';
-import 'package:test_app/src/model/diary/get_diary_list_result.dart';
+import 'package:test_app/src/controller/diary/diary_datail_controller.dart';
 import 'package:http/http.dart' as http;
 import 'package:test_app/src/model/diary/put_diary_update.dart';
 import 'package:test_app/src/model/diary/put_diary_update_result.dart';
@@ -24,22 +22,22 @@ class DiaryServices {
   final storage = const FlutterSecureStorage();
   //다이어리 Dio
 
-  Future<GetDiaryListResult> getDiary(var id) async {
-    final response = await client.get(Uri.parse("$url/user/${int.parse(id)}"),
-        headers: headers);
-    return getDiaryListResultFromJson(response.body);
-  }
+  // Future<GetDiaryListResult> getDiary(var id) async {
+  //   final response = await client.get(Uri.parse("$url/user/${int.parse(id)}"),
+  //       headers: headers);
+  //   return getDiaryListResultFromJson(response.body);
+  // }
 
-  Future<GetDiaryDetailResult> getDiaryDetail(var id) async {
-    final response = await client.get(Uri.parse("$url/${int.parse(id)}"));
-    return getDiaryDetailResultFromJson(response.body);
-  }
+  // Future<GetDiaryDetailResult> getDiaryDetail(var id) async {
+  //   final response = await client.get(Uri.parse("$url/${int.parse(id)}"));
+  //   return getDiaryDetailResultFromJson(response.body);
+  // }
 
-  Future<DeleteDiaryResult> deleteDiary(var id) async {
-    final response = await client.put(Uri.parse("$url/delete"),
-        headers: headers, body: jsonEncode(int.parse(id)));
-    return deleteDiaryResultFromJson(response.body);
-  }
+  // Future<DeleteDiaryResult> deleteDiary(var id) async {
+  //   final response = await client.put(Uri.parse("$url/delete"),
+  //       headers: headers, body: jsonEncode(int.parse(id)));
+  //   return deleteDiaryResultFromJson(response.body);
+  // }
 
   Future<PutDiaryUpdateResult> putDiary(PutDiaryUpdate diary) async {
     final response = await client.put(Uri.parse("$url/update"),
@@ -49,8 +47,6 @@ class DiaryServices {
 
   Future<Dio> diaryDio(
       var accessToken, var refreshToken, var refreshTokenExpirationTime) async {
-    Timer? timer;
-    print(accessToken);
     final options = BaseOptions(
       baseUrl: '${dotenv.env['BASE_URL']}',
       headers: {
@@ -70,6 +66,7 @@ class DiaryServices {
       },
       onResponse: (response, hanler) async {
         // 새로운 토큰 발급
+        print(response);
         if (response.data["state"] == 401) {
           final newToken = await tokenRefresh(refreshToken);
           // 새로운 토큰으로 요청을 재시도합니다.
@@ -84,15 +81,45 @@ class DiaryServices {
       },
     ));
 
-    timer = Timer.periodic(const Duration(minutes: 1), (_) {
-      final difference =
-          DateTime(refreshTokenExpirationTime).difference(DateTime.now());
-      if (difference.inMinutes <= 0) {
-        timer?.cancel();
-        storage.deleteAll();
-        Get.offAllNamed("/register");
-      }
-    });
+    return dio;
+  }
+
+  Future<Dio> diaryDetailDio(var accessToken, var refreshToken,
+      var refreshTokenExpirationTime, var diaryId) async {
+    final options = BaseOptions(
+      baseUrl: '${dotenv.env['BASE_URL']}',
+      headers: {
+        'Authorization': accessToken,
+        'Content-Type': 'application/json'
+      },
+      connectTimeout: const Duration(seconds: 5),
+      receiveTimeout: const Duration(seconds: 5),
+    );
+
+    final dio = Dio(options);
+
+    dio.interceptors.clear();
+    dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) async {
+        return handler.next(options);
+      },
+      onResponse: (response, hanler) async {
+        // 새로운 토큰 발급
+        print(response);
+        if (response.data["state"] == 401) {
+          final newToken = await tokenRefresh(refreshToken);
+          // 새로운 토큰으로 요청을 재시도합니다.
+          final request = response.requestOptions
+            ..headers['Authorization'] = 'Bearer $newToken';
+          await dio.request(request.path,
+              options: Options(headers: request.headers));
+        } else if (response.data["state"] == 200) {
+          DiaryDetailController().diaryDetailData = response.data["data"];
+          DiaryDetailController().diaryDetailData.value.diaryEmotion!.sort();
+          DiaryDetailController().diaryDetailData.value.diaryMet!.sort();
+        }
+      },
+    ));
 
     return dio;
   }
