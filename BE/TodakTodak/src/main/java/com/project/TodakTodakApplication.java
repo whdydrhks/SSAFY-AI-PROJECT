33da7d638015d8a1af18e2221db04f99cc91ab83
HyunseokCheong <hyunseokcheong@gmail.com>
2023-03-30 09:53:13 +0900

    230330_BE_CHEONG_MODI_ModifyProjectName

--- BE/TodakTodak/src/main/java/com/project/TodakTodakApplication.java
+++ BE/TodakTodak/src/main/java/com/project/TodakTodakApplication.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing // base time entity 사용을 위한 어노테이션
 @SpringBootApplication
-public class LoginApplication {
+public class TodakTodakApplication {
     
     public static void main(String[] args) {
-        SpringApplication.run(LoginApplication.class, args);
+        SpringApplication.run(TodakTodakApplication.class, args);
     }
 }
--- BE/TodakTodak/src/test/java/com/project/auth/TodakTodakApplicationTests.java
+++ BE/TodakTodak/src/test/java/com/project/auth/TodakTodakApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LoginApplicationTests {
+class TodakTodakApplicationTests {
     
     @Test
     void contextLoads() {
