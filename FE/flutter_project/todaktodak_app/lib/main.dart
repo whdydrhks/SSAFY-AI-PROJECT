b536091fbf3cfb36f1c1eea5aa3b32d457934a91
ryuwc <wom05169@gmail.com>
2023-04-02 21:59:37 +0900

    230402_FE_RYU_FEAT_TestSomeCode

--- FE/flutter_project/todaktodak_app/lib/main.dart
+++ FE/flutter_project/todaktodak_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 import 'package:test_app/src/binding/Init_binding.dart';
