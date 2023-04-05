e0bb0ad4623c60cb520759351185170b5865c1ab
김지환 <wlghks960@naver.com>
2023-04-06 02:56:00 +0900

    230406_FE_KIM_FIX_다이어리 디자인 수정 및 STT 버그 수정

--- FE/flutter_project/todaktodak_app/lib/src/components/diary/detail/diary_detail_emotion_denominator_zero_component.dart
+++ FE/flutter_project/todaktodak_app/lib/src/components/diary/detail/diary_detail_emotion_denominator_zero_component.dart
@@ -6,7 +6,7 @@ import 'package:test_app/src/config/mode.dart';
 import '../../../controller/diary/diary_datail_controller.dart';
 
 class DiaryDetailEmotionDenominatorZeroComponent extends StatelessWidget {
-  const DiaryDetailEmotionDenominatorZeroComponent({super.key});
+  const DiaryDetailEmotionDenominatorZeroComponent({super.key, Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +18,7 @@ class DiaryDetailEmotionDenominatorZeroComponent extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(
+                const SizedBox(
                   height: 8,
                 ),
                 Text("AI 감정 분석",
@@ -26,44 +26,53 @@ class DiaryDetailEmotionDenominatorZeroComponent extends StatelessWidget {
                         fontSize: 24,
                         fontFamily: 'Jua_Regular',
                         color: Mode.textMode(currentMode))),
-                SizedBox(
-                  height: 8,
-                ),
-                for (int i = 0; i < 5; i++) ...[
-                  Row(
-                    children: [
-                      Image.asset(
-                        controller.images[i].imagePath!,
-                        width: 40,
-                        height: 80,
-                      ),
-                      SizedBox(
-                        width: 12,
-                      ),
-                      Container(
-                        margin: const EdgeInsets.symmetric(vertical: 20),
-                        width: 248,
-                        height: 20,
-                        child: ClipRRect(
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(10)),
-                            child: LinearProgressIndicator(
-                                value: 0,
-                                backgroundColor: const Color(0xffD6D6D6))),
-                      ),
-                      SizedBox(
-                        width: 12,
-                      ),
-                      Text(
-                        "0%",
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontFamily: 'Jua_Regular',
-                            color: Mode.textMode(currentMode)),
-                      )
-                    ],
-                  ),
-                ],
+                Container(
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    child: Column(
+                      children: List.generate(
+                          5,
+                          (i) => Row(
+                                children: [
+                                  Image.asset(
+                                    controller.images[i].imagePath!,
+                                    width: 40,
+                                    height: 80,
+                                  ),
+                                  const SizedBox(
+                                    width: 12,
+                                  ),
+                                  const Expanded(
+                                      child: SizedBox(
+                                    height: 8,
+                                    child: ClipRRect(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(16)),
+                                      child: LinearProgressIndicator(
+                                        value: 0,
+                                        backgroundColor: Color(0xffD6D6D6),
+                                      ),
+                                    ),
+                                  )),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  SizedBox(
+                                    width:
+                                        MediaQuery.of(context).size.width / 9,
+                                    child: Align(
+                                      alignment: Alignment.centerRight,
+                                      child: Text(
+                                        "0",
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            fontFamily: 'Jua_Regular',
+                                            color: Mode.textMode(currentMode)),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              )),
+                    ))
               ],
             ),
           );
