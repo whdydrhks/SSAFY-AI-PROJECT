1de26c427e8dc558e6c11c565b5fd0cba38ea32a
류원창 <wom05169@gmail.com>
2023-05-24 09:43:04 +0900

    prepare merge

--- FE/Kim/flutter_getx/android/app/src/main/kotlin/com/example/flutter_getx/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_getx
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- FE/Kim/stt/lib/colors.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-const bgColor = Color(0xff00A67E);
-const textColor = Color(0xffFEFDFC);
